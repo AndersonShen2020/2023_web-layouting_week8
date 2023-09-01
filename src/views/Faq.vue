0549fc2c465add5bca92e012301f3c8f19817f11
m_m <minnn7716@gmail.com>
2023-09-01 21:54:20 +0800

    feat: 新增 FAQ 頁面

--- src/views/Faq.vue
+++ src/views/Faq.vue
@@ -1,3 +1,210 @@
 <template>
-  <div>Faq</div>
+  <div class="faq bg-light">
+    <div class="container font-tw pt-15 pb-20 pt-lg-16 pb-lg-30">
+      <h2
+        class="fs-h11 fs-lg-h9 fw-black letter-spacing-12 pb-10 pb-lg-16 mb-0 border-dotted-bottom"
+      >
+        FAQ
+      </h2>
+      <div class="row fw-medium">
+        <div class="col-12 col-lg-4">
+          <ul class="list-unstyled mb-0 py-4 py-lg-8 border-dotted-bottom border-bottom-lg-0">
+            <li>
+              <a
+                class="d-block text-decoration-none pt-2 pb-0 mb-2 py-2 border-bottom-primary300 border-bottom-lg-0"
+                href="#"
+                >ALPHABOX+ 使用問題</a
+              >
+            </li>
+            <li>
+              <a class="d-block text-decoration-none py-2" href="#">訂購與運送</a>
+            </li>
+            <li>
+              <a class="d-block text-decoration-none py-2" href="#">產品保養與維修</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-12 col-lg-8">
+          <div class="accordion accordion-flush" id="accordion-faq">
+            <div class="accordion-item py-4 py-lg-6">
+              <h2 class="accordion-header" id="flush-headingOne">
+                <button
+                  class="accordion-button collapsed px-0"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#flush-collapseOne"
+                  aria-expanded="false"
+                  aria-controls="flush-collapseOne"
+                >
+                  <div class="col-10 col-lg-9 fw-medium lh-base">ALPHABOX+ 的使用方式是什麼？</div>
+                  <div class="col-2 col-lg-3 text-end text-lg-start">
+                    <span class="material-icons-outlined addIcon"> add </span>
+                    <span class="material-icons-outlined closeIcon"> close </span>
+                  </div>
+                </button>
+              </h2>
+              <div
+                id="flush-collapseOne"
+                class="accordion-collapse collapse"
+                aria-labelledby="flush-headingOne"
+                data-bs-parent="#accordion-faq"
+              >
+                <div class="accordion-body col-12 col-lg-9 p-0 fw-normal">
+                  首先請確保機器人已安裝並連接至Wi-Fi，然後使用專屬的應用程式進行設定。隨後即可透過語音指令或應用程式與機器人進行互動。
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item py-4 py-lg-6">
+              <h2 class="accordion-header" id="flush-headingTwo">
+                <button
+                  class="accordion-button collapsed px-0"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#flush-collapseTwo"
+                  aria-expanded="false"
+                  aria-controls="flush-collapseTwo"
+                >
+                  <div class="col-10 col-lg-9 fw-medium lh-base">
+                    ALPHABOX+ 需要什麼樣的環境來使用？
+                  </div>
+                  <div class="col-2 col-lg-3 text-end text-lg-start">
+                    <span class="material-icons-outlined addIcon"> add </span>
+                    <span class="material-icons-outlined closeIcon"> close </span>
+                  </div>
+                </button>
+              </h2>
+              <div
+                id="flush-collapseTwo"
+                class="accordion-collapse collapse"
+                aria-labelledby="flush-headingTwo"
+                data-bs-parent="#accordion-faq"
+              >
+                <div class="accordion-body col-12 col-lg-9 p-0 fw-normal">
+                  首先請確保機器人已安裝並連接至Wi-Fi，然後使用專屬的應用程式進行設定。隨後即可透過語音指令或應用程式與機器人進行互動。
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item py-4 py-lg-6">
+              <h2 class="accordion-header" id="flush-headingThree">
+                <button
+                  class="accordion-button collapsed px-0"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#flush-collapseThree"
+                  aria-expanded="false"
+                  aria-controls="flush-collapseThree"
+                >
+                  <div class="col-10 col-lg-9 fw-medium lh-base">ALPHABOX+ 支援哪些語言？</div>
+                  <div class="col-2 col-lg-3 text-end text-lg-start">
+                    <span class="material-icons-outlined addIcon"> add </span>
+                    <span class="material-icons-outlined closeIcon"> close </span>
+                  </div>
+                </button>
+              </h2>
+              <div
+                id="flush-collapseThree"
+                class="accordion-collapse collapse"
+                aria-labelledby="flush-headingThree"
+                data-bs-parent="#accordion-faq"
+              >
+                <div class="accordion-body col-12 col-lg-9 p-0 fw-normal">
+                  首先請確保機器人已安裝並連接至Wi-Fi，然後使用專屬的應用程式進行設定。隨後即可透過語音指令或應用程式與機器人進行互動。
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item py-4 py-lg-6">
+              <h2 class="accordion-header" id="flush-headingFour">
+                <button
+                  class="accordion-button collapsed px-0"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#flush-collapseFour"
+                  aria-expanded="false"
+                  aria-controls="flush-collapseFour"
+                >
+                  <div class="col-10 col-lg-9 fw-medium lh-base">
+                    ALPHABOX+ 的軟體更新方式為何？
+                  </div>
+                  <div class="col-2 col-lg-3 text-end text-lg-start">
+                    <span class="material-icons-outlined addIcon"> add </span>
+                    <span class="material-icons-outlined closeIcon"> close </span>
+                  </div>
+                </button>
+              </h2>
+              <div
+                id="flush-collapseFour"
+                class="accordion-collapse collapse"
+                aria-labelledby="flush-headingFour"
+                data-bs-parent="#accordion-faq"
+              >
+                <div class="accordion-body col-12 col-lg-9 p-0 fw-normal">
+                  首先請確保機器人已安裝並連接至Wi-Fi，然後使用專屬的應用程式進行設定。隨後即可透過語音指令或應用程式與機器人進行互動。
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item py-4 py-lg-6">
+              <h2 class="accordion-header" id="flush-Five">
+                <button
+                  class="accordion-button collapsed px-0"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#flush-collapseFive"
+                  aria-expanded="false"
+                  aria-controls="flush-collapseFive"
+                >
+                  <div class="col-10 col-lg-9 fw-medium lh-base">
+                    購買 ALPHABOX+ 後的售後服務如何？
+                  </div>
+                  <div class="col-2 col-lg-3 text-end text-lg-start">
+                    <span class="material-icons-outlined addIcon"> add </span>
+                    <span class="material-icons-outlined closeIcon"> close </span>
+                  </div>
+                </button>
+              </h2>
+              <div
+                id="flush-collapseFive"
+                class="accordion-collapse collapse"
+                aria-labelledby="flush-Five"
+                data-bs-parent="#accordion-faq"
+              >
+                <div class="accordion-body col-12 col-lg-9 p-0 fw-normal">
+                  首先請確保機器人已安裝並連接至Wi-Fi，然後使用專屬的應用程式進行設定。隨後即可透過語音指令或應用程式與機器人進行互動。
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item py-4 py-lg-6">
+              <h2 class="accordion-header" id="flush-headingSix">
+                <button
+                  class="accordion-button collapsed px-0"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#flush-collapseSix"
+                  aria-expanded="false"
+                  aria-controls="flush-collapseSix"
+                >
+                  <div class="col-10 col-lg-9 fw-medium lh-base">
+                    ALPHABOX+ 與2他智能家居設備的相容性如何？
+                  </div>
+                  <div class="col-2 col-lg-3 text-end text-lg-start">
+                    <span class="material-icons-outlined addIcon"> add </span>
+                    <span class="material-icons-outlined closeIcon"> close </span>
+                  </div>
+                </button>
+              </h2>
+              <div
+                id="flush-collapseSix"
+                class="accordion-collapse collapse"
+                aria-labelledby="flush-headingSix"
+                data-bs-parent="#accordion-faq"
+              >
+                <div class="accordion-body col-12 col-lg-9 p-0 fw-normal">
+                  首先請確保機器人已安裝並連接至Wi-Fi，然後使用專屬的應用程式進行設定。隨後即可透過語音指令或應用程式與機器人進行互動。
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
