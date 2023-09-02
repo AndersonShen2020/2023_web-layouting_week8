88dea31c39c809750da2957a6dfcc9480ea383ea
AndersonShen <shen.andersonshen@gmail.com>
2023-09-02 16:57:08 +0800

    feat: 增加 product 的 navbar

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,3 +1,57 @@
+<script setup>
+import { onMounted, watch } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+onMounted(() => {
+  console.log(route.params.productName);
+});
+
+watch(
+  () => route.params,
+  () => {
+    console.log(route.params.productName);
+  }
+);
+</script>
+
 <template>
-  <div>Product</div>
+  <main>
+    <ul class="list-unstyled d-flex justify-content-center bg-dark py-4 gap-md-6 gap-2">
+      <li>
+        <RouterLink
+          class="btn btn-alphabox"
+          :class="{ 'btn-alphabox-active': $route.params.productName === 'Diane' }"
+          :to="`/product/Diane`"
+          >Diane</RouterLink
+        >
+      </li>
+      <li>
+        <RouterLink
+          class="btn btn-alphabox"
+          :class="{ 'btn-alphabox-active': $route.params.productName === 'Felix' }"
+          :to="`/product/Felix`"
+          >Felix</RouterLink
+        >
+      </li>
+      <li>
+        <RouterLink
+          class="btn btn-alphabox"
+          :class="{ 'btn-alphabox-active': $route.params.productName === 'Karina' }"
+          :to="`/product/Karina`"
+          >Karina</RouterLink
+        >
+      </li>
+      <li>
+        <RouterLink
+          class="btn btn-alphabox"
+          :class="{ 'btn-alphabox-active': $route.params.productName === 'Vito' }"
+          :to="`/product/Vito`"
+          >Vito</RouterLink
+        >
+      </li>
+    </ul>
+    <h1>UserID: {{ $route.params.productName }}</h1>
+  </main>
 </template>
