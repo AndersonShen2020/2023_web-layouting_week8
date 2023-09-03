6184893a2b25532f152c447a1df49d546ab1e31e
AndersonShen <shen.andersonshen@gmail.com>
2023-09-04 02:02:33 +0800

    feat: 增加 product 的 swiper 區塊

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,29 +1,78 @@
 <script setup>
-import { onMounted, watch } from "vue";
+import { onMounted, watch, ref } from "vue";
 import { useRoute } from "vue-router";
+
+// import Swiper core and required modules
+import { Navigation } from "swiper/modules";
+
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+
+// util
+import { getAssetsFile } from "@/util/pub-use";
 
 const route = useRoute();
 
-onMounted(() => {
-  console.log(route.params.productName);
-});
+const modules = [Navigation];
 
-watch(
-  () => route.params,
-  () => {
-    console.log(route.params.productName);
+const productInfo = {
+  Diane: {
+    type: "知識型",
+    do: "學術領導/知識分享/智能問答",
+    photo1: "diane_square1.png",
+    photo2: "diane_square2.png"
+  },
+  Felix: {
+    type: "娛樂型",
+    do: "音樂播放/語音互動/遊戲陪伴",
+    photo1: "felix_square1.png",
+    photo2: "felix_square2.png"
+  },
+  Karina: {
+    type: "生活型",
+    do: "健康提醒/日程安排/智能家居",
+    photo1: "karina_square1.png",
+    photo2: "karina_square2.png"
+  },
+  Vito: {
+    type: "情感型",
+    do: "情感識別/心情分析/心靈支持",
+    photo1: "vito_square1.png",
+    photo2: "vito_square2.png"
   }
-);
+};
+
+const swiperDom = ref(null);
+function setSlideToFirst() {
+  // 當變更產品時，Swiper 會跳回到第一張圖片
+  swiperDom.value.$el.swiper.slideTo(0);
+}
+
+// onMounted(() => {
+//   console.log(swiperDom);
+// });
+
+// watch(
+//   () => route.params,
+//   () => {
+//     console.log(route.params.productName);
+//   }
+// );
 </script>
 
 <template>
   <main>
-    <ul class="list-unstyled d-flex justify-content-center bg-dark py-4 gap-md-6 gap-2">
+    <ul class="list-unstyled d-flex justify-content-center bg-dark py-4 gap-md-6 gap-2 mb-0">
       <li>
         <RouterLink
           class="btn btn-alphabox"
           :class="{ 'btn-alphabox-active': $route.params.productName === 'Diane' }"
           :to="`/product/Diane`"
+          @click="setSlideToFirst"
         >
           Diane
         </RouterLink>
@@ -33,6 +82,7 @@ watch(
           class="btn btn-alphabox"
           :class="{ 'btn-alphabox-active': $route.params.productName === 'Felix' }"
           :to="`/product/Felix`"
+          @click="setSlideToFirst"
         >
           Felix
         </RouterLink>
@@ -42,6 +92,7 @@ watch(
           class="btn btn-alphabox"
           :class="{ 'btn-alphabox-active': $route.params.productName === 'Karina' }"
           :to="`/product/Karina`"
+          @click="setSlideToFirst"
         >
           Karina
         </RouterLink>
@@ -51,11 +102,47 @@ watch(
           class="btn btn-alphabox"
           :class="{ 'btn-alphabox-active': $route.params.productName === 'Vito' }"
           :to="`/product/Vito`"
+          @click="setSlideToFirst"
         >
           Vito
         </RouterLink>
       </li>
     </ul>
-    <h1>UserID: {{ $route.params.productName }}</h1>
+    <!-- <h1>UserID: {{ $route.params.productName }}</h1> -->
+    <!-- <p>
+      {{ $route.params.productName }} - {{ productInfo[$route.params.productName].type }} -
+      {{ productInfo[$route.params.productName].do }}
+    </p> -->
+    <div class="d-flex flex-column flex-lg-row">
+      <div class="product-swiper-side">
+        <Swiper
+          :modules="modules"
+          :slides-per-view="1"
+          :space-between="50"
+          :navigation="true"
+          ref="swiperDom"
+        >
+          <SwiperSlide>
+            <img
+              class="object-fit-cover"
+              :src="getAssetsFile(productInfo[$route.params.productName].photo1)"
+              :alt="`${$route.params.productName}-1`"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              class="object-fit-cover"
+              :src="getAssetsFile(productInfo[$route.params.productName].photo2)"
+              :alt="`${$route.params.productName}-2`"
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+      <div class="product-info-side">
+        <p>{{ $route.params.productName }}</p>
+        <p>{{ productInfo[$route.params.productName].type }}</p>
+        <p>{{ productInfo[$route.params.productName].do }}</p>
+      </div>
+    </div>
   </main>
 </template>
