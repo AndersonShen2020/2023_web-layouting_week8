d0bf91edf84d2f39ced9e38226a78ece1afbfebb
AndersonShen <shen.andersonshen@gmail.com>
2023-09-04 21:36:14 +0800

    feat: 增加 product 的說明區塊

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -52,6 +52,7 @@ function setSlideToFirst() {
 
 <template>
   <main>
+    <!-- Navbar -->
     <ul class="list-unstyled d-flex justify-content-center bg-dark py-4 gap-md-6 gap-2 mb-0">
       <li>
         <RouterLink
@@ -94,6 +95,7 @@ function setSlideToFirst() {
         </RouterLink>
       </li>
     </ul>
+    <!-- Swiper -->
     <div class="d-flex flex-column flex-lg-row bg-dark text-white">
       <div class="product-swiper-side">
         <Swiper
@@ -131,5 +133,81 @@ function setSlideToFirst() {
         />
       </div>
     </div>
+    <!-- 學術領導/知識分享/智能問答 -->
+    <div class="container font-tw mt-lg-20 mt-15 mb-lg-30 mb-8">
+      <div class="row">
+        <div class="col-12 d-flex justify-content-between align-items-center fw-medium lh-base">
+          <h2 class="d-flex align-items-center fs-16 mb-0">
+            <span class="material-symbols-outlined fs-11 me-3"> emergency </span
+            >學術領導/知識分享/智能問答
+          </h2>
+          <p class="mb-0 d-none d-lg-block">ALPHABOX+</p>
+        </div>
+      </div>
+      <div class="row mt-lg-8 mt-6">
+        <div class="col-lg-4 col-12">
+          <p class="fw-medium mb-0">
+            歡迎來到ALPHABOX+的全新世界！在這裡，我們很榮幸地向您介紹我們的最新力作 -
+            3D立體投影機器人 Felix！
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="container overflow-hidden font-tw mt-lg-30 mt-8 mb-lg-30 mb-20">
+      <ul class="row gy-lg-26 gy-6 list-unstyled">
+        <li class="col-lg-4 col-12">
+          <h3 class="fs-17 fw-bold lh-base mb-0 border-lg-0 border-bottom border-2 border-black">
+            先進的3D投影技術
+          </h3>
+          <p class="mt-lg-6 mt-4 mb-0">
+            {{ $route.params.productName }} 搭載了最新的 3D
+            投影技術，能夠將虛擬影像清晰地呈現在您的現實空間中，讓您感受身臨其境的體驗！
+          </p>
+        </li>
+        <li class="col-lg-4 col-12">
+          <h3 class="fs-17 fw-bold lh-base mb-0 border-lg-0 border-bottom border-2 border-black">
+            人性化的互動設計
+          </h3>
+          <p class="mt-lg-6 mt-4 mb-0">
+            {{
+              $route.params.productName
+            }}
+            具有高度智能的語音辨識和回應功能，您可以像跟朋友聊天一樣，與
+            {{ $route.params.productName }}
+            進行自然、流暢的對話。
+          </p>
+        </li>
+        <li class="col-lg-4 col-12">
+          <h3 class="fs-17 fw-bold lh-base mb-0 border-lg-0 border-bottom border-2 border-black">
+            豐富的應用場景
+          </h3>
+          <p class="mt-lg-6 mt-4 mb-0">
+            無論是家庭娛樂、教育培訓還是商業展示，{{ $route.params.productName }}
+            都能夠輕鬆滿足您的需求，為您提供更多元的互動體驗。
+          </p>
+        </li>
+        <li class="col-lg-4 col-12">
+          <h3 class="fs-17 fw-bold lh-base mb-0 border-lg-0 border-bottom border-2 border-black">
+            高度自主性
+          </h3>
+          <p class="mt-lg-6 mt-4 mb-0">
+            Felix
+            具有強大的自主學習能力，能夠不斷地學習並優化自己，為您創造更加便捷、貼心的生活方式。
+          </p>
+        </li>
+        <li class="col-lg-4 col-12">
+          <h3 class="fs-17 fw-bold lh-base mb-0 border-lg-0 border-bottom border-2 border-black">
+            簡單易用的操作界面
+          </h3>
+          <p class="mt-lg-6 mt-4 mb-0">
+            {{
+              $route.params.productName
+            }}
+            擁有直觀的操作界面，讓您可以輕鬆地掌握並運用它的各種功能。
+          </p>
+        </li>
+      </ul>
+    </div>
+    <!-- 使用分享 -->
   </main>
 </template>
