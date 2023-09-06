1914d5cfe25189c86f186012bf377806611e5bd5
homer <horn9002@gmail.com>
2023-09-06 20:11:01 +0800

    update: index & aboutus

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -15,32 +15,6 @@
         <li class="nav-item "><RouterLink to="#" class="nav-link active">常見問題</RouterLink></li>
         <li class="nav-item "><RouterLink to="#" class="nav-link active">隱私政策</RouterLink></li>
       </ul>
-
     </div>
-
-
-
-
-      <!-- <ul class="navbar-nav gap-10 align-items-center">
-        <li class="nav-item "><RouterLink to="#" class="nav-link active text-white">產品</RouterLink></li>
-        <li class="nav-item "><RouterLink to="#" class="nav-link active text-white">關於我們</RouterLink></li>
-        <li class="nav-item "><RouterLink to="#" class="nav-link active text-white">最新消息</RouterLink></li>
-        <li class="nav-item "><RouterLink to="#" class="nav-link active text-white">FAQ</RouterLink></li>
-        <li class="nav-item "><RouterLink to="#" class="nav-link active text-white">聯絡我們</RouterLink></li>
-      </ul>
-      <ul class="navbar-nav gap-6 align-items-center">
-        <li class="nav-item "><RouterLink to="#" class="nav-link active text-white ">註冊</RouterLink></li>
-        <li class="nav-item "><RouterLink to="#" class="nav-link active text-white ">登入</RouterLink></li>
-        <li class="nav-item "><RouterLink to="#" class="nav-link active btn rounded-1 text-center bg-white px-4"><span class="icon material-symbols-outlined">point_scan</span>訂閱</RouterLink></li>
-      </ul>
-    </div> -->
-
-
-
-
-
-
-
-
   </footer>
 </template>
--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,3 +1,63 @@
 <template>
-  <div>AboutUs</div>
+  <div class="aboutus-title text-white font-tw py-16 mb-12">
+    <div class="container mb-12">
+      <h3 class="fs-12 letter-spacing-5 mb-6">關於我們</h3>
+      <div class="row">
+        <div class="col-md-6">
+          <p>創立於2021年，我們的使命是將科技與人性化設計相結合，為您打造最佳的3D立體投影陪伴機器人。憑藉創新的技術、卓越的研發團隊以及對品質的追求，我們致力於為全球用戶提供最先進且易用的陪伴機器人。</p>
+        </div>
+        <div class="col-md-6 text-end">
+          <p class="mb-0">我們擁抱變革，追求技術創新，以提供更卓越的產品給全球用戶。</p>
+          <p class="mb-0">我們始終以用戶需求為核心，致力於提供最滿意的產品與服務。</p>
+          <p class="mb-0">我們堅持高品質標準，確保每一款產品都經過嚴格的測試與品質把控。</p>
+          <p class="mb-0">我們重視團隊協作，鼓勵創意交流，共同為實現願景而努力。</p>
+        </div>
+      </div>
+    </div>
+    <img src="../assets/img/about_us.png" alt="about_us" class="w-100 mb-10">
+    <div class="container">
+      <div class="w-75 mx-auto d-flex align-items-center">
+        <div class="material-icons fs-6 me-5">emergency</div>
+        <p class="mb-0">我們相信科技的力量能夠改變人類生活。通過不斷創新與優化，我們期待為您打造一款具有高度智能、豐富功能且呈現真實感的3D立體投影陪伴機器人，讓科技成為您生活中真誠的夥伴。 </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="aboutus-content mx-auto font-tw">
+    <div class="top pb-10">
+      <div class="row">
+        <img src="../assets/img/hand.png" alt="" class="col-md-4">
+        <img src="../assets/img/screen.png" alt="" class="col-md-4">
+        <img src="../assets/img/box.png" alt="" class="col-md-4">
+      </div>
+      <h2 class="text-center fw-bold my-5">ALPHABOX+</h2>
+
+      <div class="row">
+        <div class="col-md-6">
+          <p>我們提供多款3D立體投影陪伴機器人，涵蓋知識型、娛樂型、生活型和情感型等不同類別，以滿足您的各種需求。每款產品都經過精心設計，具備高度智能化、多功能和人性化操作等特點，讓您在日常生活中獲得更便利的陪伴體驗。</p>
+        </div>
+        <div class="col-md-6 text-end">
+          <p>此外，我們還提供完善的售後服務，包括產品保固、技術支援及線上客服<br>協助，確保您在購買與使用過程中得到全面支持。</p>
+        </div>
+      </div>
+    </div>
+    <div class="container d-flex align-items-center py-16">
+
+      <img src="../assets/img/meeting.png" alt="" class="w-50 me-6">
+
+      <p class="w-50">展望未來，我們將繼續投入研發，引領3D立體投影陪伴機器人技術的發展。通過結合虛擬現實、擴增現實和人工智能等領域的最新技術，我們將努力為用戶帶來更多前所未有的創新體驗。</p>
+
+    </div>
+  </div>
+  <div class="aboutus-recruit bg-black py-6 font-tw">
+    <div class="container">
+      <div class="w-75 d-flex justify-content-between align-items-center mx-auto text-white">
+        <p class="w-75 mb-0 me-md-6">我們熱忱歡迎有志之士加入我們的團隊，共同創造更美好的未來。如果您對科技充滿熱情，並擁有相關領域的專業知識，請隨時查看我們的招聘信息或與我們聯繫，讓我們攜手共進。</p>
+        <buttom class="btn btn-white rounded-1 text-center w-25"><span
+            class="icon material-symbols-outlined">point_scan</span>查看職缺</buttom>
+      </div>
+    </div>
+
+
+  </div>
 </template>
