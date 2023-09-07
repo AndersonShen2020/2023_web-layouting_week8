0c263109731d3b147305013e99e902b47838c196
christophe <christophe790601@gmail.com>
2023-09-07 20:45:21 +0800

    Fix f/chris diverged problem

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,34 @@
 <template>
-  <header>
-    <RouterLink class="nav-link" aria-current="page" to="/"> index </RouterLink>
-    <RouterLink class="nav-link" aria-current="page" to="/product/Diane"> product </RouterLink>
-    <RouterLink class="nav-link" aria-current="page" to="/about"> about </RouterLink>
-    <RouterLink class="nav-link" aria-current="page" to="/news"> news </RouterLink>
-    <RouterLink class="nav-link" aria-current="page" to="/Faq"> Faq </RouterLink>
-    <RouterLink class="nav-link" aria-current="page" to="/ContactUs"> ContactUs </RouterLink>
-    <RouterLink class="nav-link" aria-current="page" to="/SignUp"> SignUp </RouterLink>
-    <RouterLink class="nav-link" aria-current="page" to="/SignIn"> SignIn </RouterLink>
+  <header class="navbar navbar-expand-md bg-primary border-bottom border-white font-tw">
+    <div class="container d-flex">
+      <RouterLink to="#" class="navbar-brand text-white fs-13 p-0 font-en">ALPHABOX+</RouterLink>
+      <ul class="navbar-nav gap-10 align-items-center">
+        <li class="nav-item "><RouterLink to="#" class="nav-link active text-white">產品</RouterLink></li>
+        <li class="nav-item "><RouterLink to="#" class="nav-link active text-white">關於我們</RouterLink></li>
+        <li class="nav-item "><RouterLink to="#" class="nav-link active text-white">最新消息</RouterLink></li>
+        <li class="nav-item "><RouterLink to="#" class="nav-link active text-white">FAQ</RouterLink></li>
+        <li class="nav-item "><RouterLink to="#" class="nav-link active text-white">聯絡我們</RouterLink></li>
+      </ul>
+      <ul class="navbar-nav gap-6 align-items-center">
+        <li class="nav-item "><RouterLink to="#" class="nav-link active text-white ">註冊</RouterLink></li>
+        <li class="nav-item "><RouterLink to="#" class="nav-link active text-white ">登入</RouterLink></li>
+        <li class="nav-item "><RouterLink to="#" class="nav-link active btn rounded-1 text-center bg-white px-4"><span class="icon material-symbols-outlined">point_scan</span>訂閱</RouterLink></li>
+      </ul>
+    </div>
+
+
+      <button class="navbar-toggler border-0 d-flex d-lg-none p-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <div class="icon-custom">
+          <span class="icon-line"></span>
+          <span class="icon-line"></span>
+          <span class="icon-line"></span>
+        </div>
+      </button>
+
+
+
+
+
+
   </header>
 </template>
--- src/views/index.vue
+++ src/views/index.vue
@@ -542,11 +542,53 @@ const eventsSm = [
       <p class="index-share-bgText position-absolute">ALPHABOX+</p>
     </section>
     <!-- section5 -->
-
+    <div class="section5">
+      <div class="container">
+        <h2 class="fs-10 text-white py-10 m-0">聯絡我們</h2>
+      </div>
+    </div>
     <!-- section6 -->
-    <RouterLink class="nav-link" aria-current="page" to="/product/Diane"> Diane </RouterLink>
+
+    <div class="section6 row m-0">
+      <div class="card border-end bg-black text-white col-lg-3 col-6 rounded-0">
+        <div class="card-body px-8 py-12">
+          <p class="card-title text-center font-number title-fs">1</p>
+          <p class="card-title text-center fs-13 mb-13">Diane</p>
+          <p class="card-text">知識型<br>學術領導/知識分享/智能問答</p>
+        </div>
+      </div>
+
+      <div class="card border-end bg-black text-white col-lg-3 col-6 rounded-0">
+        <div class="card-body px-8 py-12">
+          <p class="card-title text-center font-number title-fs">2</p>
+          <p class="card-title text-center fs-13 mb-13">Felix</p>
+          <p class="card-text">娛樂型<br>音樂播放/語音互動/遊戲陪伴</p>
+        </div>
+      </div>
+
+      <div class="card border-end bg-black text-white col-lg-3 col-6 rounded-0">
+        <div class="card-body px-8 py-12">
+          <p class="card-title text-center font-number title-fs">3</p>
+          <p class="card-title text-center fs-13 mb-13">Karina</p>
+          <p class="card-text">生活型<br>健康提醒/日程安排/智能家居</p>
+        </div>
+      </div>
+
+      <div class="card border-end bg-black text-white col-lg-3 col-6 rounded-0">
+        <div class="card-body px-8 py-12">
+          <p class="card-title text-center font-number title-fs">4</p>
+          <p class="card-title text-center fs-13 mb-13">Vito</p>
+          <p class="card-text">情感型<br>情感識別/心情分析/心靈支持</p>
+        </div>
+      </div>
+
+    </div>
+
+
+    <!-- <RouterLink class="nav-link" aria-current="page" to="/product/Diane"> Diane </RouterLink>
     <RouterLink class="nav-link" aria-current="page" to="/product/Felix"> Felix </RouterLink>
     <RouterLink class="nav-link" aria-current="page" to="/product/Karina"> Karina </RouterLink>
-    <RouterLink class="nav-link" aria-current="page" to="/product/Vito"> Vito </RouterLink>
+    <RouterLink class="nav-link" aria-current="page" to="/product/Vito"> Vito </RouterLink> -->
+
   </main>
 </template>
